398eeccee0dd3e65212c24470518cc723d53b3ea
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2022-10-23 13:22:02 -0500

    removed validation

--- pages/api/order/currently.js
+++ pages/api/order/currently.js
@@ -7,11 +7,11 @@ export default async function handler(req,res){
    if (method === "POST"){
     
     let response = await db.query("Select id,description,precio from pedido where status = $1 and user_id = $2",['no entregado',body.cedula]);
-    let rs =  await db.query("Select * from usuario where cedula = $1",[body.cedula])
-    console.log(rs)
+    
+    
     let resto = []
     response.rows.map(e =>{
-      resto.push(e.id +" , "+ e.description+ " , "+e.precio+ " , "+ rs.rows[0].saldo)
+      resto.push(e.id +" , "+ e.description+ " , "+e.precio)
     })
     res.send(resto);
    }
