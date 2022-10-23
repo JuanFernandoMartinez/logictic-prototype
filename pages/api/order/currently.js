597bea96f5715af764eb6cd1beec0ad14afc5168
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2022-10-23 11:31:50 -0500

    configured order api return

--- pages/api/order/currently.js
+++ pages/api/order/currently.js
@@ -8,6 +8,9 @@ export default async function handler(req,res){
     
     let response = await db.query("Select id,description,precio from pedido where status = $1 and user_id = $2",['no entregado',body.cedula]);
     
-    res.send(response.rows);
+    let resto = response.rows.map(e =>{
+      e.id + e.description
+    })
+    res.send(resto);
    }
 }
