e15a4bc174fc3afecc5669e7835d572770275b28
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2022-10-23 12:35:16 -0500

    corrected saldo function

--- pages/api/order/currently.js
+++ pages/api/order/currently.js
@@ -7,10 +7,11 @@ export default async function handler(req,res){
    if (method === "POST"){
     
     let response = await db.query("Select id,description,precio from pedido where status = $1 and user_id = $2",['no entregado',body.cedula]);
-    let rs =  await db.query("Select saldo from usuario where cedula = $1",[body.cedula])
+    let rs =  await db.query("Select * from usuario where cedula = $1",[body.cedula])
+    console.log(rs)
     let resto = []
     response.rows.map(e =>{
-      resto.push(e.id +" , "+ e.description+ " , "+e.precio+ " , "+rs.rows[0])
+      resto.push(e.id +" , "+ e.description+ " , "+e.precio+ " , "+ rs.rows[0].saldo)
     })
     res.send(resto);
    }
