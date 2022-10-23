ea16f013864296fea6757d314efb46d79bc38021
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2022-10-23 12:06:33 -0500

    added user id  send data

--- pages/api/order/currently.js
+++ pages/api/order/currently.js
@@ -7,10 +7,10 @@ export default async function handler(req,res){
    if (method === "POST"){
     
     let response = await db.query("Select id,description,precio from pedido where status = $1 and user_id = $2",['no entregado',body.cedula]);
-    
+    let rs =  await db.query("Select saldo from usuario where cedula = $1",[body.cedula])
     let resto = []
     response.rows.map(e =>{
-      resto.push(e.id +" , "+ e.description+ " , "+e.precio)
+      resto.push(e.id +" , "+ e.description+ " , "+e.precio+ " , "+rs.rows[0].cedula)
     })
     res.send(resto);
    }
