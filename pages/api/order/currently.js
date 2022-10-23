726af80634672c7486ed0272aa3f2c7f424fa8e2
Juan Fernando Martinez Dominguez <jm1811324@gmail.com>
2022-10-23 11:50:03 -0500

    changed segment

--- pages/api/order/currently.js
+++ pages/api/order/currently.js
@@ -10,7 +10,7 @@ export default async function handler(req,res){
     
     let resto = []
     response.rows.map(e =>{
-      resto.push(e.id +" "+ e.description+ " "+e.precio)
+      resto.push(e.id +" , "+ e.description+ " , "+e.precio)
     })
     res.send(resto);
    }
